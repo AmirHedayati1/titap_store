8d1c14260e4f8a558c851e8c19db85c2536842b8
Amir Hedayati <hedayatiamirh1@gmail.com>
2026-06-29 10:33:12 +0330

    feat: Add custom user model

--- backend/src/users/admin.py
+++ backend/src/users/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from users.models import User
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+class UserAdmin(BaseUserAdmin):
+    model = User
+    list_display = ['email', 'id']
+    search_fields = ['email']
+
+admin.site.register(User, UserAdmin)
